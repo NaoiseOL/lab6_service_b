f4cd503795ac9703b409011d197579ad5d0ad339
NaoiseOL <118356809+NaoiseOL@users.noreply.github.com>
2025-11-28 11:45:01 +0000

    Updated main, docker and ci workflow

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-from sqlalchemy.orm import Session
-from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
+from fastapi import FastAPI
+import httpx, os
 
-from app.database import engine, SessionLocal
-from app.models import Base, UserDB
-from app.schemas import UserCreate, UserRead
+app = FastAPI(title="Service B - Proxy API")
+SERVICE_A_BASE_URL = os.getenv("SERVICE_A_BASE_URL", "http://localhost:8001")
 
-app = FastAPI()
-Base.metadata.create_all(bind=engine)
+@app.get("/api/proxy-greet")
+def call_service_a(name: str = "world"):
+    url = f"{SERVICE_A_BASE_URL}/api/greet/{name}"
+    with httpx.Client() as client:
+        r = client.get(url)
+    return {"service_b": True, "service_a_response": r.json()}
 
